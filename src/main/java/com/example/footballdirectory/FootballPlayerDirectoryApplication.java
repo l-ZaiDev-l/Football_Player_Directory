0f8ff46195844c1746fdfda9031d9b7a660f7658
ZaidDev <zaidhankri2@gmail.com>
2024-05-21 10:14:21 +0100

    First Main commit

---
+++ src/main/java/com/example/footballdirectory/FootballPlayerDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballPlayerDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballPlayerDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/footballdirectory/FootballPlayerDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballdirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballPlayerDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
