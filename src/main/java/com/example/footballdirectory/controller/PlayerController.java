c2e3ef40153a5e86b1aabefbc31af5165d706df6
ZaidDev <zaidhankri2@gmail.com>
2024-05-24 00:09:46 +0100

    Players details v1

--- src/main/java/com/example/footballdirectory/controller/PlayerController.java
+++ src/main/java/com/example/footballdirectory/controller/PlayerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 @Controller
@@ -21,5 +22,11 @@ public class PlayerController {
         List<PlayerDto> players = playerService.findAllPlayers();
         model.addAttribute("players",players);
         return "players";
+    }
+    @GetMapping("/players/{playerId}")
+    public String playerDetails(@PathVariable("playerId") long playerId, Model model) {
+        PlayerDto playerDto = playerService.findPlayerById(playerId);
+        model.addAttribute("player", playerDto);
+        return "single";
     }
 }
